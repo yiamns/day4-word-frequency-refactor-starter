c5362c911362d8499ea9d71e4121e955323dc130
ethan <ethan.yang@oocl.com>
2025-09-04 16:13:39 +0800

    refactor:rename map to group

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -33,10 +33,10 @@ public class WordFrequencyGame {
     }
 
     private List<Input> countFrequencies(String[] words) {
-        Map<String, List<String>> map = groupSameWords(words);
+        Map<String, List<String>> group = groupSameWords(words);
 
         List<Input> frequencies = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry : map.entrySet()){
+        for (Map.Entry<String, List<String>> entry : group.entrySet()){
             Input input = new Input(entry.getKey(), entry.getValue().size());
             frequencies.add(input);
         }
